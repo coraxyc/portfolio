7d7d8fa685bf5b7cfda107d323590268518436e3
coraxyc <ycxing99@gmail.com>
2019-07-06 23:35:44 -0700

    Added about body text holder and styling

--- src/AboutSection.js
+++ src/AboutSection.js
@@ -18,7 +18,20 @@ class WorkSection extends Component {
                                 </div>
                         </div>
                     </div>
+                    <div class="About-body">
+                        Insert some kind of bio that makes a lot of sense because 
+                        sometimes other things don’t.
 
+                        For example, how the hell I’m going to make this website
+                        isn’t making sense right now, but I hope I’m going to figure
+                        this out.
+
+                        Inserter des types de bio qui faitent beaucoup du sense
+                        que parfois les autres choses ne faitent pas de sense. Par
+                        exemple, comment l’hell je vais créer cette site ne pas faire
+                        de sense maintenant, mais œ∑´®†¥¨ˆøπåß∂ƒ©˙∆˚¬Ω≈ç√∫˜µ
+                        πøˆºª•∞¶∞¢£™¡™£¢∞∞§
+                    </div>
                 </div>
             </section>
         )
