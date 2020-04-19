64e799fa3b2674835a8c73f5326c210a51bf1b4b
coraxyc <ycxing99@gmail.com>
2020-04-18 20:36:16 -0700

    add public url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 render((
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
   ), document.getElementById('root'));
