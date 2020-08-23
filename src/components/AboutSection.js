a05c248f18c4628b5a8db9593742187cc5a65695
coraxyc <ycxing99@gmail.com>
2020-08-22 20:06:44 -0700

    add visual design section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,67 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useCallback } from 'react'
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
-class WorkSection extends Component {
-    render() {
-        return (
-            <section className="About" id="about">
-                <div className="About-container">
-                    <div className="About-circle">
-                        <div className="About-circle-body">
-                            <div className="About-circle-body-title">
-                                Other work
-                            </div>
-                            <div className="About-circle-body-divider"></div>
-                                <div className="circle-body-text">
-                                    Other projects I’ve worked on, including software, design, to phototography.
-                                    <br/><br/>
-                                    Currently a work in progress.
-                                </div>
-                        </div>
+import EspressoBanner from "../images/visual_design/espresso_banner.png";
+import HackXXBanner from "../images/visual_design/hackxx_banner.png";
+import HackXXInfo from "../images/visual_design/hackxx_info.png";
+import SDHacksBanner from "../images/visual_design/sdhacks_banner.png";
+import EOTGBanner from "../images/visual_design/tesc_eotg_flyer.png";
+import YonderKickstarterBanner from "../images/visual_design/yonder_kickstarter_tiers.png";
+import SeeMoreWork from '../images/visual_design/see_more_work.png';
+
+const photos = [
+    { src: EspressoBanner, width: 1, height: 0.5 },
+    { src: HackXXInfo, width: 1, height: 1.5 },
+    { src: EOTGBanner, width: 1, height: 1.2 },
+    { src: SDHacksBanner, width: 1, height: 2.2 },
+    { src: HackXXBanner, width: 1, height: 2.2 },
+    { src: YonderKickstarterBanner, width: 1, height: 2.2 },
+    { src: SeeMoreWork, width: 1, height: 0.5 },
+]
+
+const WorkSection = () => {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+
+    return (
+        <section className="About" id="about">
+            <div className="About-container">
+                <div className="About-title">
+                    <div className="Work-circle-body-title">
+                        Visual Design
                     </div>
-                    <div className="About-body">
-                        work in progress! a few links will appear here eventually
-                    </div>
+                    <div className="About-circle-body-divider"></div>
                 </div>
-            </section>
-        )
-    }
+                <div className="About-body">
+                    <Gallery photos={photos} direction={"column"} onClick={openLightbox} />
+                    <ModalGateway>
+                        {viewerIsOpen ? (
+                            <Modal onClose={closeLightbox}>
+                                <Carousel
+                                    currentIndex={currentImage}
+                                    views={photos.map(x => ({
+                                        ...x,
+                                        srcset: x.srcSet,
+                                        caption: x.title
+                                    }))}
+                                />
+                            </Modal>
+                        ) : null}
+                    </ModalGateway>
+                </div>
+            </div>
+        </section>
+    );
 }
 export default WorkSection
--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -10,73 +10,73 @@ class ProjectSection extends Component {
             <section className="Work" id="work">
                 <div className="Work-container">
                     <div className="horizontal">
-                    <div className="Work-info">
-                        <div className="Work-info-container">
-                            <Link to="./projects/informed">
-                                <img className="Work-info-image" src={InformedDisplay}/>
-                            </Link>
-                            <div className="Work-info-text">
-                                <p className="project-title">
-                                    iNFORMed
+                        <div className="Work-info">
+                            <div className="Work-info-container">
+                                <Link to="./projects/cardconnect">
+                                    <img className="Work-info-image" src={CardConnectDisplay} />
+                                </Link>
+                                <div className="Work-info-text">
+                                    <p className="project-title">
+                                        CardConnect
                                 </p>
-                                <p className="project-subtitle">
-                                    UI/UX Design, Visual Design, User Research
+                                    <p className="project-subtitle">
+                                        Front-end Web Development, UI Design, UX Research
                                 </p>
-                                <p className="project-body">
-                                    Grassroots community platform streamlining access to verified COVID-19 resources.
+                                    <p className="project-body">
+                                        Digital contacts list tool that allows you to create templated business cards.
                                 </p>
-                            </div>
-                        </div>
-                        <div className="Work-info-container">
-                            <Link to="./projects/cardconnect">
-                                <img className="Work-info-image" src={CardConnectDisplay}/>
-                            </Link>
-                            <div className="Work-info-text">
-                                <p className="project-title">
-                                    CardConnect
-                                </p>
-                                <p className="project-subtitle">
-                                    Front-end Web Development, UI Design, UX Research
-                                </p>
-                                <p className="project-body">
-                                    Digital contacts list tool that allows you to create templated business cards.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="Work-info-container">
-                            <Link to="./projects/avantgarde">
-                                <img className="Work-info-image" src={AvantGardeDisplay}/>
-                            </Link>
-                            <div className="Work-info-text">
-                                <p className="project-title">
-                                    Avant Garde Poster Design
-                                </p>
-                                <p className="project-subtitle">
-                                    Information Design, Visual Design, UX Research
-                                </p>
-                                <p className="project-body">
-                                Iterative study on grids, typography, and information organization.
-                                </p>
-                            </div>
-                        </div>
-                        {/* to make big circle scroll to almost the end*/}
-                        <div className="margin-top-bottom-5em"></div>
-                    </div>
-                    <div className="Work-title-body">
-                        <div className="Work-circle">
-                            <div className="Work-circle-body">
-                                <div className="Work-circle-body-title">
-                                    Projects
                                 </div>
-                                <div className="Work-circle-body-divider"></div>
-                                <div className="Work-circle-body-text">
-                                    Here’s a few projects that I’d like to showcase.
-                                    <br/><br/>
+                            </div>
+                            <div className="Work-info-container">
+                                <Link to="./projects/informed">
+                                    <img className="Work-info-image" src={InformedDisplay} />
+                                </Link>
+                                <div className="Work-info-text">
+                                    <p className="project-title">
+                                        iNFORMed
+                                </p>
+                                    <p className="project-subtitle">
+                                        UI/UX Design, Visual Design, User Research
+                                </p>
+                                    <p className="project-body">
+                                        Grassroots community platform streamlining access to verified COVID-19 resources.
+                                </p>
+                                </div>
+                            </div>
+                            <div className="Work-info-container">
+                                <Link to="./projects/avantgarde">
+                                    <img className="Work-info-image" src={AvantGardeDisplay} />
+                                </Link>
+                                <div className="Work-info-text">
+                                    <p className="project-title">
+                                        Avant Garde Poster Design
+                                </p>
+                                    <p className="project-subtitle">
+                                        Information Design, Visual Design, UX Research
+                                </p>
+                                    <p className="project-body">
+                                        Iterative study on grids, typography, and information organization.
+                                </p>
+                                </div>
+                            </div>
+                            {/* to make big circle scroll to almost the end*/}
+                            <div className="margin-top-bottom-5em"></div>
+                        </div>
+                        <div className="Work-title-body">
+                            <div className="Work-circle">
+                                <div className="Work-circle-body">
+                                    <div className="Work-circle-body-title">
+                                        Projects
+                                    </div>
+                                    <div className="Work-circle-body-divider"></div>
+                                    <div className="Work-circle-body-text">
+                                        Here’s a few projects that I’d like to showcase.
+                                    <br /><br />
                                     Tap on the images to view case studies.
                                 </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </section>
